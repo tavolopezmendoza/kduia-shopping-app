02721c4e94b2fe4b1f2620c4dfe7b487e0b8b0a6
Gustavo López Mendoza <tavolopezmendoza@gmail.com>
2023-11-04 16:29:00 -0300

    Edit file for shooping app

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,7 +1,27 @@
-import React, { useContext } from 'react';
+﻿import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Location = () => {
+    const { dispatch } = useContext(AppContext);
+
+    const changeLocation = (val) => {
+        dispatch({
+            type: 'CHG_LOCATION',
+            payload: val,
+        })
+    }
+
+    return (
+        <div className='alert alert-secondary'>Location {
+            <select name="Location" id="Location" onChange={event => changeLocation(event.target.value)}>
+                <option value="a">UK(a)</option>
+                <option value="a">India(a)</option>
+                <option value="a">Europe(a)</option>
+                <option value="a">Canada(a)</option>
+            </select>
+        }
+        </div>
+    );
 };
 
 export default Location;
